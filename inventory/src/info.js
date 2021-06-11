ed0f6a48f05fbefc37945fb6e5a2fd7f2446e811
Varun Kumar <varunjha089@gmail.com>
2021-06-11 11:43:34 +0530

    Moving from function to class based components.

--- inventory/src/info.js
+++ inventory/src/info.js
@@ -1,13 +1,37 @@
-export function Info() {
-	const title = "This is the title of Applicaton";
-	const showTitle = true;
+// Class based Components
 
-	return (
-		<div>
-			<h1>{title ? showTitle : ""}</h1>
-			<p>Manage your stuff.</p>
-		</div>
-	);
+import React from "react";
+
+class Info extends React.Component {
+  render() {
+    const title = "This is the title of Applicaton";
+    const showTitle = true;
+
+    if (showTitle) {
+      return (
+        <div>
+          <h1>{title}</h1>
+          <p>Manage your stuff</p>
+        </div>
+      );
+    } else {
+      return <p>empty</p>;
+    }
+  }
 }
+
+// Function based Components
+
+// export function Info() {
+// 	const title = "This is the title of Applicaton";
+// 	const showTitle = true;
+
+// 	return (
+// 		<div>
+// 			<h1>{title ? showTitle : ""}</h1>
+// 			<p>Manage your stuff.</p>
+// 		</div>
+// 	);
+// }
 
 export default Info;
