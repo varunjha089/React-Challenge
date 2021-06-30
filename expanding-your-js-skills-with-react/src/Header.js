d92d8a6c37bb40695e27b20224c768bc4cf43e5c
Varun Kumar <varunjha089@gmail.com>
2021-06-30 05:49:54 +0000

    Header and Item component updated.

--- expanding-your-js-skills-with-react/src/Header.js
+++ expanding-your-js-skills-with-react/src/Header.js
@@ -1,9 +1,7 @@
 import React from "react";
 
-class Header extends React.Component {
-  render() {
-    return <h1>Todos:</h1>;
-  }
-}
+const Header = () => {
+  return <h1>Todos:</h1>;
+};
 
 export default Header;
--- expanding-your-js-skills-with-react/src/Item.js
+++ expanding-your-js-skills-with-react/src/Item.js
@@ -1,16 +1,14 @@
 import React from "react";
 
-class Item extends React.Component {
-  render() {
-    return (
-      <li>
-        <label>
-          <input type="checkbox" checked={this.props.done || false} />
-          <span>{this.props.text}</span>
-        </label>
-      </li>
-    );
-  }
-}
+const Item = (props) => {
+  return (
+    <li>
+      <label>
+        <input type="checkbox" checked={props.done || false} />
+        <span>{props.text}</span>
+      </label>
+    </li>
+  );
+};
 
 export default Item;
